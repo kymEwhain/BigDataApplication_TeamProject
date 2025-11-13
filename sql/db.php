2db8595d379b1b7ad3e52a1e3ec3f344d0216d1b
kymEwhain <ymdwang200@ewhain.net>
2025-11-13 13:18:37 +0900

    Update db.php
    modify mysql_connect "test01"->"team01"

--- sql/db.php
+++ sql/db.php
@@ -1,11 +1,12 @@
 <?php
 
 function connectDB() {
-  $mysqli = mysqli_connect("localhost", "test01", "test01", "test01");
+  $mysqli = mysqli_connect("localhost", "team01", "team01", "team01");
   if (mysqli_connect_errno()) {
     die("ERROR: Could not connect DB. " . mysqli_connect_error());
   }
   return $mysqli;
 }
+
 
 ?>
